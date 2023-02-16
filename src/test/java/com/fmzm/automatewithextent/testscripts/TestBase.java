a7447b6b349c0309f55e3fdd9a3e3c019bad0dde
Fawad Malik <fawad.malik@hotmail.com>
2023-02-15 23:29:37 -0800

    POC of basic extent report functionality localized to one test class

--- src/test/java/com/fmzm/automatewithextent/testscripts/TestBase.java
+++ src/test/java/com/fmzm/automatewithextent/testscripts/TestBase.java
@@ -1,5 +1,7 @@
 package com.fmzm.automatewithextent.testscripts;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.testsmith.support.listeners.*;
 import com.fmzm.automatewithextent.utils.BrowserUtil;
 import com.fmzm.automatewithextent.utils.ScreenshotListener;
@@ -18,6 +20,8 @@ public abstract class TestBase {
         return driver.get();
     }
 
+    ExtentReports extent;
+
     @BeforeClass
     public void setup() {
         final String browser = System.getProperty("browser", "chrome");
@@ -30,9 +34,29 @@ public abstract class TestBase {
         ).decorate(originalDriver));
     }
 
+    @BeforeClass
+    public void configExtent(){
+        //extentreports , extentsparkrepoprter
+        String path = System.getProperty("user.dir") + "\\reports\\index.html";
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(path);
+
+        sparkReporter.config().setReportName("Web Automation Results");
+        sparkReporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+
+        extent.setSystemInfo("Tester", "Fawad Malik");
+    }
+
     @AfterClass
     public void teardown() {
         getDriver().quit();
+    }
+
+    @AfterClass
+    public void teardownExtent(){
+        extent.flush();
     }
 
 }
--- src/test/java/com/fmzm/automatewithextent/testscripts/ToolshopTest.java
+++ src/test/java/com/fmzm/automatewithextent/testscripts/ToolshopTest.java
@@ -1,22 +1,45 @@
 package com.fmzm.automatewithextent.testscripts;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.fmzm.automatewithextent.pages.HomePage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Test
 public class ToolshopTest extends TestBase {
 
+    @Test
     public void verifySignInButton() {
+        ExtentTest test = extent.createTest("verifySignInButton");
         HomePage homePage = new HomePage(getDriver());
         homePage.get();
-        assertThat(homePage.isSignInButtonDisplayed()).isTrue();
+        try {
+            assertThat(homePage.isSignInButtonDisplayed()).isTrue();
+            test.pass("homePage.isSignInButtonDisplayed()).isTrue()");
+        }
+        catch (AssertionError ae){
+            test.fail(ae.getMessage());
+        }
+
     }
 
+    @Test
     public void verifySignInButton_failOnPurpose() {
+        ExtentTest test = extent.createTest("verifySignInButton_failOnPurpose");
         HomePage homePage = new HomePage(getDriver());
         homePage.get();
-        assertThat(homePage.isSignInButtonDisplayed()).isFalse();
+
+        try {
+            assertThat(homePage.isSignInButtonDisplayed()).isFalse();
+            test.pass("homePage.isSignInButtonDisplayed()).isFalse()");
+        }
+        catch (AssertionError ae){
+            test.fail(ae.getMessage());
+        }
+
     }
 }
