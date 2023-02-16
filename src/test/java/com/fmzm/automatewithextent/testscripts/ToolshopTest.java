a2fb0ea8d70349fdc9b96b0cebc0acfdfb1e8fd3
Fawad Malik <fawad.malik@hotmail.com>
2023-02-15 22:46:17 -0800

    First commit of skeleton with Allure reports.

---
+++ src/test/java/com/fmzm/automatewithextent/testscripts/ToolshopTest.java
@@ -0,0 +1,22 @@
+package com.fmzm.automatewithextent.testscripts;
+
+import com.fmzm.automatewithextent.pages.HomePage;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Test
+public class ToolshopTest extends TestBase {
+
+    public void verifySignInButton() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.get();
+        assertThat(homePage.isSignInButtonDisplayed()).isTrue();
+    }
+
+    public void verifySignInButton_failOnPurpose() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.get();
+        assertThat(homePage.isSignInButtonDisplayed()).isFalse();
+    }
+}
---
+++ src/test/java/com/fmzm/automatewithextent/utils/Browser.java
@@ -0,0 +1,5 @@
+package com.fmzm.automatewithextent.utils;
+
+public enum Browser {
+    CHROME, FIREFOX, EDGE, CHROME_HEADLESS, FIREFOX_HEADLESS
+}
